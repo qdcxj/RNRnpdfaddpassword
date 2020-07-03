180710128d2b85d447a80533af7078b6da9f140b
ChenXiangJun <Xiang-Jun.Chen@pccw.com>
2020-07-03 14:39:04 +0800

    add

--- ios/RNRnpdfaddpassword.m
+++ ios/RNRnpdfaddpassword.m
@@ -8,12 +8,14 @@
 #import <React/RCTUtils.h>
 
 
-@implementation RNRnpdfaddpassword
+@implementation RNRnpdfaddpassword {
     RCTPromiseResolveBlock _resolveBlock;
     RCTPromiseRejectBlock _rejectBlock;
+}    
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(convert:(NSDictionary *)options) {
+RCT_EXPORT_METHOD(convert:(NSDictionary *)options, resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
 
       NSString *fileName = [RCTConvert NSString:options[@"fileName"]];
       NSString *password = [RCTConvert NSString:options[@"password"]];
