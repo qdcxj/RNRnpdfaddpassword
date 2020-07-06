50be6a0d25a431dff832567a31098ebbf01e8b7f
ChenXiangJun <Xiang-Jun.Chen@pccw.com>
2020-07-06 16:52:42 +0800

    11

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 import { NativeModules } from 'react-native';
-let {RNMyLibrary} = NativeModules
+let {RNMyLibrary,RNHTMLtoPDFa} = NativeModules
 console.log(RNMyLibrary)
 export default {
-    RNMyLibrary
+    RNMyLibrary,RNHTMLtoPDFa
 }
 
