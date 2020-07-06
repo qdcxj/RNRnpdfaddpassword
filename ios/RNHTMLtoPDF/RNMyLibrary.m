d1576b76d50c19f849467ba2279d831b3fa5d652
ChenXiangJun <Xiang-Jun.Chen@pccw.com>
2020-07-06 15:05:20 +0800

    11

--- ios/RNHTMLtoPDF/RNMyLibrary.m
+++ ios/RNHTMLtoPDF/RNMyLibrary.m
@@ -1,30 +1,5 @@
-
-#import <CoreGraphics/CoreGraphics.h>
-#import <UIKit/UIKit.h>
-#import <React/RCTConvert.h>
-#import <React/RCTEventDispatcher.h>
-#import <React/RCTView.h>
-#import <React/UIView+React.h>
-#import <React/RCTUtils.h>
 #import "RNMyLibrary.h"
-@implementation RNMyLibrary{
-    RCTEventDispatcher *_eventDispatcher;
-    RCTPromiseResolveBlock _resolveBlock;
-    RCTPromiseRejectBlock _rejectBlock;
-    NSString *_html;
-    NSString *_fileName;
-    NSString *_filePath;
-    UIColor *_bgColor;
-    NSInteger *_numberOfPages;
-    CGSize _PDFSize;
-    UIWebView *_webView;
-    float _paddingBottom;
-    float _paddingTop;
-    float _paddingLeft;
-    float _paddingRight;
-    BOOL _base64;
-    BOOL autoHeight;
-}
+@implementation RNMyLibrary
 RCT_EXPORT_MODULE();
 RCT_EXPORT_METHOD(testCallback:(RCTResponseSenderBlock)callback)
 {
