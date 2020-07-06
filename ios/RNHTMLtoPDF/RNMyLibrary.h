9d5b9ddf6c28f88ec94a3912decdc373ad972a65
ChenXiangJun <Xiang-Jun.Chen@pccw.com>
2020-07-06 16:39:14 +0800

    11

--- ios/RNHTMLtoPDF/RNMyLibrary.h
+++ ios/RNHTMLtoPDF/RNMyLibrary.h
@@ -1,10 +1,4 @@
-#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
-#elif __has_include("React/RCTBridgeModule.h")
-#import "React/RCTBridgeModule.h"
-#else
-#import "RCTBridgeModule.h"
-#endif
 
 @interface RNMyLibrary : NSObject <RCTBridgeModule>
 
