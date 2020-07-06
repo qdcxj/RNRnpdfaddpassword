33958791fe55bda5913c352ca0c094afe62b3242
ChenXiangJun <Xiang-Jun.Chen@pccw.com>
2020-07-06 15:25:43 +0800

    no message

--- ios/RNHTMLtoPDF/RNMyLibrary.m
+++ ios/RNHTMLtoPDF/RNMyLibrary.m
@@ -1,10 +1,10 @@
 #import "RNMyLibrary.h"
 @implementation RNMyLibrary
 RCT_EXPORT_MODULE();
-RCT_EXPORT_METHOD(testCallback:(RCTResponseSenderBlock)callback)
-{
-    NSArray *items = @[@"callback ", @"test ", @"array"];
-    callback(@events);
+RCT_EXPORT_METHOD(nativeLog:(id)obj) {
+  NSLog(@"开始输出日志：");
+  NSLog(@"%@",obj);
+  NSLog(@"日志输出完毕！");
 }
 @end
   
