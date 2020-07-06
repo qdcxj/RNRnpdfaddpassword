729164c8e4fdb3b9dafaa8a56aec02ce971a98d5
ChenXiangJun <Xiang-Jun.Chen@pccw.com>
2020-07-06 15:40:17 +0800

    11

--- ios/RNHTMLtoPDF/RNMyLibrary.h
+++ ios/RNHTMLtoPDF/RNMyLibrary.h
@@ -1,13 +1,14 @@
-
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#import <React/RCTView.h>
-#else
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
-#import <React/RCTView.h>
+#elif __has_include("React/RCTBridgeModule.h")
+#import "React/RCTBridgeModule.h"
+#else
+#import "RCTBridgeModule.h"
 #endif
 
 @interface RNMyLibrary : NSObject <RCTBridgeModule>
+
++ (NSString *)env;
 
 @end
   
--- ios/RNHTMLtoPDF/RNMyLibrary.m
+++ ios/RNHTMLtoPDF/RNMyLibrary.m
@@ -1,10 +1,16 @@
 #import "RNMyLibrary.h"
 @implementation RNMyLibrary
-RCT_EXPORT_MODULE();
-RCT_EXPORT_METHOD(nativeLog:(id)obj) {
-  NSLog(@"开始输出日志：");
-  NSLog(@"%@",obj);
-  NSLog(@"日志输出完毕！");
+
+RCT_EXPORT_MODULE()
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
 }
+
++ (NSString *)env {
+    return @"dssf";
+}
+
 @end
   
